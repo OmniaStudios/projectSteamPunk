b0cdca416e7588bbbb53df55e523e6b5baedfd2c
Sabadell Mattia <34937314+ElSabadell@users.noreply.github.com>
2021-01-09 14:37:55 +0100

    Started the home page. Finished the nav

---
+++ public/assets/js/script.js
@@ -0,0 +1,26 @@
+// var moveDiv = document.getElementById('cursor_custom');
+
+//  window.onmousemove = function (e) {
+//      var x = e.pageX,
+//          y = e.pageY;
+//      moveDiv.style.top = (y + 0) + 'px';
+//      moveDiv.style.left = (x + 0) + 'px';
+//  };
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
